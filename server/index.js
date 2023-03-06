49d4d813d599bb4f78ab3010ce3fdfbac825bb75
Lin Liu <lin_6066@yahoo.com>
2023-03-06 17:37:46 -0500

    fixed note controller

--- server/index.js
+++ server/index.js
@@ -16,10 +16,15 @@ console.log(process.env.NODE_ENV);
 connectDB();
 
 app.use(logger);
+
 app.use(cors(corsOptions));
+
 app.use(express.json());
+
 app.use(cookieParser());
+
 app.use("/", express.static(path.join(__dirname, "public")));
+
 app.use("/", require("./routes/root"));
 app.use("/users", require("./routes/userRoutes"));
 app.use("/notes", require("./routes/noteRoutes"));
